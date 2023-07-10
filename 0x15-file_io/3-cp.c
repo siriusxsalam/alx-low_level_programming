d2ead63ca4b525811661f3452c4001027c4a79ca
SALAM Hamza <salam.hamza967@gmail.com>
2023-07-10 16:33:36 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -56,27 +56,27 @@ void close_file(int fd)
  *              If file_to cannot be created or written to - exit code 99.
  *              If file_to or file_from cannot be closed - exit code 100.
  */
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
 	int f, t, re, wr;
 	char *buf;
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buf = create_buffer(av[2]);
-	f = open(av[1], O_RDONLY);
+	buf = create_buffer(argv[2]);
+	f = open(argv[1], O_RDONLY);
 	re = read(f, buf, 1024);
-	t = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (f == -1 || re == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", av[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buf);
 			exit(98);
 		}
@@ -85,13 +85,13 @@ int main(int ac, char *av[])
 		if (t == -1 || wr == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", av[2]);
+				"Error: Can't write to %s\n", argv[2]);
 			free(buf);
 			exit(99);
 		}
 
 		re = read(f, buf, 1024);
-		t = open(av[2], O_WRONLY | O_APPEND);
+		t = open(argg[2], O_WRONLY | O_APPEND);
 
 	} while (re > 0);
 
